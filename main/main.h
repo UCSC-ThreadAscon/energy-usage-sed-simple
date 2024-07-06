e067252c72b9259629c633656e21510d69e51951
Simeon Tran <siatran@ucsc.edu>
2024-07-06 12:33:32 -0700

    Empty Memory helper.

--- main/main.h
+++ main/main.h
@@ -1,3 +1,18 @@
 #pragma once
 
 #include "init.h"
+
+/**
+ * Empties all memory for `size` bytes starting at memory address `pointer`.
+ *
+ * @param[in] pointer: the pointer of the stack memory
+ * @param[in] size:    the size of the memory that `pointer` points to
+ *
+ * I got the idea to use `memset()` to clear stack memory from
+ * the Google Search AI:
+ * https://docs.google.com/document/d/1o-NaEOA-vzWPCv7VX1dONUfwos2epveDk4H_Y2Y5g1Y/edit?usp=sharing
+*/
+static inline void EmptyMemory(void* pointer, size_t size) {
+  memset(pointer, 0, size);
+  return;
+}

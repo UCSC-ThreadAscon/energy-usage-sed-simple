5297b194c7f0c45e136e9997443e00ab4ae32e81
Simeon Tran <siatran@ucsc.edu>
2024-07-06 13:59:24 -0700

    Added time API

--- main/main.h
+++ main/main.h
@@ -55,3 +55,23 @@ BatteryPayload;
 
 EventPayload createEventPayload(uuid deviceId);
 BatteryPayload createBatteryPayload(uuid deviceId);
+
+#define US_TO_MS(us) us / 1000
+#define MS_TO_US(ms) ms * 1000
+
+#define MS_TO_SECONDS(ms) ms / 1000
+#define MS_TO_MINUTES(ms) MS_TO_SECONDS(ms) / 60
+#define US_TO_MINUTES(us) MS_TO_MINUTES(US_TO_MS(us))
+#define US_TO_SECONDS(us) MS_TO_SECONDS(US_TO_MS(us))
+
+#define SECONDS_TO_MS(secs) secs * 1000
+#define SECONDS_TO_US(secs) MS_TO_US(SECONDS_TO_MS(secs))
+
+#define MINUTES_TO_SECONDS(mins) mins * 60
+#define SECONDS_TO_MINUTES(secs) secs / 60
+
+uint64_t toUs(struct timeval time);
+struct timeval getTimevalNow();
+struct timeval getFutureTimeval(uint64_t seconds);
+uint64_t timeDiffMs(struct timeval tv1, struct timeval tv2);
+uint64_t timeDiffUs(struct timeval tv1, struct timeval tv2);
---
+++ main/time.c
@@ -0,0 +1,48 @@
+/**
+ * All of the code in the documention is both taken from and based upon the code
+ * from the "System Time" page from the ESP-IDF documentation:
+ * https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/system_time.html
+*/
+
+#include "main.h"
+#include "assert.h"
+
+uint64_t toUs(struct timeval time)
+{
+  return (uint64_t)time.tv_sec * 1000000L + (uint64_t)time.tv_usec;
+}
+
+struct timeval getTimevalNow()
+{
+  struct timeval tvNow;
+  gettimeofday(&tvNow, NULL);
+  return tvNow;
+}
+
+/**
+ * What will be the timestamp "[insert number here]" seconds from now?
+*/
+struct timeval getFutureTimeval(uint64_t seconds)
+{
+  struct timeval tvFuture;
+  gettimeofday(&tvFuture, NULL);
+  tvFuture.tv_sec += seconds;
+  return tvFuture;
+}
+
+/**
+ * The code to find the duration of time between two timevals comes from:
+ * https://github.com/espressif/esp-idf/blob/master/examples/openthread/ot_sleepy_device/deep_sleep/main/esp_ot_sleepy_device.c#L117
+ *
+ * This function assumes that "tv2" occurs later than "tv1". The function
+ * returns the time elapsed starting at "tv1" to "tv2" in milliseconds.
+*/
+uint64_t timeDiffMs(struct timeval tv1, struct timeval tv2)
+{
+  return (tv2.tv_sec - tv1.tv_sec) * 1000 + (tv2.tv_usec - tv1.tv_usec) / 1000;
+}
+
+uint64_t timeDiffUs(struct timeval tv1, struct timeval tv2)
+{
+  return toUs(tv2) - toUs(tv1);
+}

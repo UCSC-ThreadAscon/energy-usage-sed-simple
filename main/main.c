200a59d01d48e0149e28ba915b905d32a0553e80
Simeon Tran <siatran@ucsc.edu>
2024-07-06 12:37:01 -0700

    -m

--- main/main.c
+++ main/main.c
@@ -1,5 +1,15 @@
 #include "main.h"
 
+/**
+ * TODO:
+ *
+ * 1. Send a Battery packet, then go to deep sleep for 30 seconds once getting response.
+ *
+ * 2. Then, calculate the time it takes to send the Battery packet and receive a response.
+ *    Go to sleep for:
+ *        30 - (time it takes to wake up, send Battery packet, and receive response).
+ */
+
 void app_main(void)
 {
   init();

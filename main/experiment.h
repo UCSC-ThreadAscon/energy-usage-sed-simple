d435c2fb8e96d1baac3989ff7a6c18379a5e4e34
Simeon Tran <siatran@ucsc.edu>
2024-07-06 16:30:52 -0700

    Create an option to send event packets every wakeup

--- main/experiment.h
+++ main/experiment.h
@@ -1,13 +1,20 @@
 #pragma once
 
-#define NUM_EVENTS_FRONT_DOOR 365
+#define NUM_EVENTS_FRONT_DOOR 36
 #define NUM_EVENTS_AIR_MONITOR 10
 #define NUM_EVENTS_BACK_DOOR 2
+
+/**
+ * Send an event packet every wakeup. Used for debugging purposes.
+ */
+#define NUM_EVENTS_DEBUG 365
 
 #if (CONFIG_SCENARIO == 1)
 #define NUM_EVENTS NUM_EVENTS_FRONT_DOOR
 #elif (CONFIG_SCENARIO == 2)
 #define NUM_EVENTS NUM_EVENTS_AIR_MONITOR
-#else
+#elif (CONFIG_SCENARIO == 3)
 #define NUM_EVENTS NUM_EVENTS_BACK_DOOR
+#else
+#define NUM_EVENTS NUM_EVENTS_DEBUG
 #endif

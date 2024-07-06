1952909a173513d4d761e947b9cf698817bfb56a
Simeon Tran <siatran@ucsc.edu>
2024-07-06 16:49:22 -0700

    Cite code

--- main/time.c
+++ main/time.c
@@ -7,6 +7,10 @@
 #include "main.h"
 #include "assert.h"
 
+/**
+ * This function comes from the ESP-IDF documentation:
+ * https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/system_time.html#get-current-time
+ */
 uint64_t toUs(struct timeval time)
 {
   return (uint64_t)time.tv_sec * 1000000L + (uint64_t)time.tv_usec;

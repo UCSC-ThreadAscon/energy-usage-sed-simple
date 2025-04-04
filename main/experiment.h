3ca7998bc8167fbb762c4664f4cae389528d6843
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2025-04-03 21:33:32 -0700

    Third device will be a window sensor, not a backdoor sensor.

--- main/experiment.h
+++ main/experiment.h
@@ -15,7 +15,7 @@ void printVariables(void);
  */
 #define NUM_EVENTS_FRONT_DOOR 36
 #define NUM_EVENTS_AIR_MONITOR 10
-#define NUM_EVENTS_BACK_DOOR 2
+#define NUM_EVENTS_WINDOW 2
 
 /**
  * Send an event packet every wakeup. Used for debugging purposes.
@@ -27,7 +27,7 @@ void printVariables(void);
 #elif (CONFIG_SCENARIO == 2)
 #define NUM_EVENTS NUM_EVENTS_AIR_MONITOR
 #elif (CONFIG_SCENARIO == 3)
-#define NUM_EVENTS NUM_EVENTS_BACK_DOOR
+#define NUM_EVENTS NUM_EVENTS_WINDOW
 #else
 #define NUM_EVENTS NUM_EVENTS_DEBUG
 #endif
